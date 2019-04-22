b99566efd5e45616716568c863480013c91ce0aa
artemaminov <artemaminov@gmail.com>
2019-04-22 14:43:35 +0500

    add before_action

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,10 +1,20 @@
 class DogsController < ApplicationController
+  before_action :get_menu_items, :get_background
+
   def index
+    @background = Preference.background
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+    @background = (@dog.background.file unless @dog.background.blank?) || Preference.background
+  end
+
+  private
+
+  def get_menu_items
     @dogs = Dog.adults.alive
     @puppies = Dog.puppies.alive
   end
 
-  def show
-    dog = Dog.find(params[:id])
-  end
 end
