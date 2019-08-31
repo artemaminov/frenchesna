4cd5068cfe671179f099bdb83f80ba0afeb5d6c2
artemaminov <artemaminov@gmail.com>
2019-09-01 00:01:10 +0500

    add gender icons

--- app/helpers/dogs_helper.rb
+++ app/helpers/dogs_helper.rb
@@ -1,2 +1,7 @@
 module DogsHelper
+  def svg_tag(name, options)
+    file_path = "#{Rails.root}/app/assets/images/#{name}.svg"
+    return content_tag(:span, File.read(file_path).html_safe, options) if File.exists?(file_path)
+    '(not found)'
+  end
 end
