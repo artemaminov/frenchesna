013aa7820ff816bf597af4d99bc9806e2295714b
artemaminov <artemaminov@gmail.com>
2020-07-09 07:47:18 +0500

    add globalize gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,24 @@
 class ApplicationController < ActionController::Base
+
+  before_action :set_locale
+
   def get_menu_items
     @dogs = Dog.adults.alive
     @puppies = Dog.puppies
   end
+
+  private
+
+  def set_locale
+    detected_locale = request.subdomains.first
+    I18n.locale = I18n.available_locales.map(&:to_s).include?(detected_locale) ? detected_locale : (browser_locale || I18n.default_locale)
+  end
+
+  def browser_locale
+    locales = request.env['HTTP_ACCEPT_LANGUAGE'] || ''
+    locales.scan(/[a-z]{2}(?=\;)/).find do |locale|
+      I18n.available_locales.include?(locale.to_sym)
+    end
+  end
+
 end
