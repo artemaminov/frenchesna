bf0328bc9a286b55e6eb8d513611668b1b1cda62
artemaminov <artemaminov@gmail.com>
2021-04-04 21:34:45 +0300

    sort dogs by breed, birth, name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def get_menu_items
-    @dogs = Dog.adults.alive
-    @puppies = Dog.puppies
+    @dogs = Dog.adults.alive.sort_by_breed.sort_by_birth.sort_by_name
+    @puppies = Dog.puppies.sort_by_breed.sort_by_birth.sort_by_name
   end
 
   private
