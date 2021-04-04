46d5de6568611b7635a639a97b778e6ba4107a48
artemaminov <artemaminov@gmail.com>
2021-04-04 19:28:55 +0300

    restore images

--- db/migrate/20210404162055_restore_images.rb
+++ db/migrate/20210404162055_restore_images.rb
@@ -2,8 +2,8 @@ class RestoreImages < ActiveRecord::Migration[5.2]
   def change
     Image.find_each do |image|
       image.viewable_type_scope = image.viewable_type == 'Picture' ? 'Gallery' : image.viewable_type
+      image.viewable_type = 'Dog'
       image.save
     end
-    Image.update_all viewable_type: 'Dog'
   end
 end
